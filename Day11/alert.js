0ea5e90c77cee10929e11510a882dde480e3ce17
Akshay C A <136839322+Akshay-C-A@users.noreply.github.com>
2024-08-23 09:56:20 +0530

    Added day 11

---
+++ Day11/alert.js
@@ -0,0 +1,29 @@
+document.writeln("<h2 style='color:blue;'>Hai</h2>")
+
+// alert("This is JavaScript")
+// a=5;b=6; c=a+b
+// window.alert(c)
+
+// var s= 'This is a string'
+// s[1] = 'H'
+// alert(s)
+
+
+// var person =  prompt("Enter you name","")
+// txt = "Hello "+ person + " Welcome!"
+// alert(txt)
+
+// var person =  prompt("Enter you name","")
+// if(person=="")
+// {
+//     txt="Enter you name"
+// }
+// else if(person==null)
+// {
+//     txt="You pressed cancel"
+// }
+// else
+// {
+//     txt = "Hello "+ person + " Welcome!"
+// }
+// alert(txt)
